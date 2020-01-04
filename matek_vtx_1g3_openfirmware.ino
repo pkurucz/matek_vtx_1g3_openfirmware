9f75f8a4b27642f7b52eb54dca7c8fda28ad67a1
Paul Kurucz <pkurucz@gmail.com>
2020-01-04 14:47:14 -0500

    Initial commit

---
+++ matek_vtx_1g3_openfirmware.ino
@@ -0,0 +1,32 @@
+/* LED Anode - Set HIGH to light LED */
+#define LED_CH4 PA0
+#define LED_CH5 PA1
+#define LED_CH6 PA2
+#define LED_CH7 PA3
+#define LED_CH8 PA4
+#define LED_CH9 PA5
+#define LED_CHH PA6
+#define LED_CHC PA7
+#define LED_CHD PB0
+
+/* Led Cathode, set LOW to light LED */
+#define LED_K1 PB8
+
+/* VTX channel selection GPIO */
+#define VTX_CS1 PA8
+#define VTX_CS2 PA11
+#define VTX_CS3 PA12
+#define VTX_S1 PA15
+
+/* User pushbutton */
+#define KEY1 PF0
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
